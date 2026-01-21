ebacef9dfd4fe3550c4b95111a0615d5fb9fba6c
Kavia AI <production@kavia.ai>
2026-01-21 10:43:16 +0000

    Checkpoint 1 - cg2626b755

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/simple-calculator-app-203188-203197/calculator_mobile_frontend
+flutter analyze
+
---
+++ calculator_mobile_frontend/android/app/src/main/kotlin/com/example/calculator_mobile_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.calculator_mobile_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
